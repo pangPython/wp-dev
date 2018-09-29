2397bfdf39f0a6a4011ef1844fcbc8a2a007b04b
pangPython <pangpython@qq.com>
2018-09-29 22:49:02 +0800

    add layui js and css

---
+++ theme/wp-layui/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+//引入css和js
+function add_css_js(){
+  //css
+  wp_enqueue_style( 'layui_css', get_template_directory_uri() . '/css/layui.css');
+  wp_enqueue_style( 'layui_mobile_css', get_template_directory_uri() . '/css/layui.mobile.css');
+  // js
+  wp_enqueue_script( 'layui_all_js', get_template_directory_uri() . '/js/layui.all.js');
+}
+add_action( 'wp_enqueue_scripts', 'add_css_js' );
+
+?>
